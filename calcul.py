e55f013f5526e494c3840f6411bc61b84c595ed2
zeqotibo <thibaultderouin5@gmail.com>
2023-10-27 09:32:02 +0200

    first commit

---
+++ calcul.py
@@ -0,0 +1,27 @@
+EUR = 0.85
+JPY = 110
+GBP = 0.75
+
+print("Bienvenue dans le Convertisseur de Devises en PYTHON.")
+print("Que voulez-vous faire?")
+print("\n1-Conversions USD/EUR", "\n2-Conversions USD/JPY", "\n3-Conversions USD/GBP")
+choix = input()
+print("Vous avez choisi", choix)
+
+if choix == '1':
+    print("Entrez le nombre d'USD que vous voulez convertir en euros:")
+    value = float(input())
+    euros = value * EUR
+    print("Montant équivalent en EUR :", euros)
+
+elif choix == '2':
+    print("Entrez le nombre d'USD que vous voulez convertir en yen:")
+    value = float(input())
+    yen = value * JPY
+    print("Montant équivalent en JPY :", yen)
+
+elif choix == '3':
+    print("Entrez le nombre d'USD que vous voulez convertir en livres:")
+    value = float(input())
+    livres = value * GBP
+    print("Montant équivalent en GBP :", livres)
